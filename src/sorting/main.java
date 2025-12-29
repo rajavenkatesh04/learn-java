3c0f146f043bae4482c7f25faeecc4d3d32a2617
rajavenkatesh04 <grv.9604@gmail.com>
2025-12-29 20:20:53 +0530

    insertion sort

--- src/sorting/main.java
+++ src/sorting/main.java
@@ -5,8 +5,21 @@ import java.util.Arrays;
 public class main {
     public static void main(String[] args) {
         int[] arr = {3, 1, 5, 4, 2};
-        selection(arr);
+        insertion(arr);
         System.out.println(Arrays.toString(arr));
+    }
+
+    static void insertion(int[] arr) {
+        for(int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i - 1;
+
+            while( j >= 0 && arr[j] > temp) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = temp;
+        }
     }
 
     static void selection(int[] arr) {
